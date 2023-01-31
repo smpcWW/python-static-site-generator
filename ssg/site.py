738a322a71610568d8369e5506fb1a35c4ce89b9
Scott Campbell <Scott.Campbell@motion.com>
2023-01-31 14:42:24 -0600

    Initial commit

--- ssg/site.py
+++ ssg/site.py
@@ -5,7 +5,7 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
     def create_dir(self, path):
-        directory = self.dest / self.path.relative_to(self.source)
+        directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True,exist_ok=True)
     def build(self):
         self.dest.mkdir(parents=True,exist_ok=True)
