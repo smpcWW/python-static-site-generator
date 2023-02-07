19a0aa40739ab0ac8ce5d3bb377ce725b721c6b4
Scott Campbell <Scott.Campbell@motion.com>
2023-02-07 09:20:40 -0600

    Submitting Module 2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,3 +3,8 @@ from typing import List
 
 class Parser:
     extensions : List[str] = []
+    def valid_extension(self,extension):
+        if extension == self.extenstions.rglob("*"):
+            return True
+        else:
+            pass
