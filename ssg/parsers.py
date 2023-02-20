5d8ff458bedbe90e6f8cd5fe6a2856879e7145d4
Scott Campbell <Scott.Campbell@motion.com>
2023-02-20 13:50:49 -0600

    Adding read funtion

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -18,9 +18,15 @@ class Parser:
         return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
+        # This method is not implemented and raises a NotImplementedError.
+        # It is intended to parse a file at the given path and write the parsed output
+        # to a new file at the given destination path.
         raise NotImplementedError
     
     def read(self, path):
+        # This method reads the contents of a file at the given path and returns the contents as a string.
+        # The file is opened in read mode using the open() function and the resulting file object is
+        # automatically closed when the with block is exited. The contents of the file are read using
+        # the read() method of the file object, and returned from the function.
         with open(path, 'r') as file:
-            contents = file.read()
-            return contents
+            return file.read()
